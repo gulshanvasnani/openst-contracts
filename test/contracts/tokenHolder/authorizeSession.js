5a7f3afcd45f72000358b873fc40bfae1f82f953
Gulshan Vasnani <gulshan@ost.com>
2018-08-31 18:21:43 +0530

    Added for execute rule

--- test/contracts/tokenHolder/authorizeSession.js
+++ test/contracts/tokenHolder/authorizeSession.js
@@ -20,76 +20,116 @@
 // ----------------------------------------------------------------------------
 
 const web3 = require('../../lib/web3'),
-	BigNumber = require('bignumber.js'),
-	Hasher = artifacts.require('./Hasher.sol'),
-	brandedToken = artifacts.require('./BrandedToken.sol');
-	tokenHolder = artifacts.require('./TokenHolder.sol');
+  BigNumber = require('bignumber.js'),
+  Hasher = artifacts.require('./Hasher.sol'),
+  web3Utils = require('web3-utils');
+brandedToken = artifacts.require('./BrandedToken.sol');
+tokenHolder = artifacts.require('./TokenHolder.sol');
 
 module.exports.perform = (accounts) => {
-	const openSTProtocol = accounts[0],
-		conversionRateDecimals = 5,
-		conversionRate = new BigNumber(10 * 10 ** conversionRateDecimals),
-		chainIDValue = 3,
-		chainIDUtility = 1410,
-		symbol = 'symbol',
-		name = 'name',
-		organizationAddress = accounts[1],
-		token = null;
-	let tokenHolderInstance,newAccount,blockNumber;
-	beforeEach(async () => {
-		
-		const hasher = await Hasher.new();
-		const tokenRules = accounts[0];
-		const valueToken = accounts[1];
-		brandedTokenInstance = await brandedToken.new(
-			valueToken,
-			symbol,
-			name,
-			18,
-			chainIDValue,
-			chainIDUtility,
-			conversionRate,
-			conversionRateDecimals,
-			organizationAddress,
-			{ from: openSTProtocol }
-		);
-	
-	});
-	
-	it('should deploy token holder with two requirements', async () => {
-		
-		tokenHolderInstance = await tokenHolder.new(brandedTokenInstance.address, accounts[3], accounts[1], 2, [accounts[7],accounts[6]]);
-		
-		assert.equal(await tokenHolderInstance.isWallet(accounts[6]),true);
-		assert.equal(await tokenHolderInstance.isWallet(accounts[4]),false);
-	});
-	
-	it('should authorizeSession for an ephemeral key with two requirements ', async () => {
-		
-		newAccount = web3.eth.accounts.create(web3.utils.randomHex(32));
-		let transactionId = await tokenHolderInstance.authorizeSession.call(newAccount.address, 100, await web3.eth.getBlockNumber() + 50,{from: accounts[7]});
-	
-		blockNumber = await web3.eth.getBlockNumber();
-		
-		// Transaction proposed and requirement achieved is 1.
-		await tokenHolderInstance.authorizeSession(newAccount.address, 100, blockNumber + 50,{from: accounts[7]});
-		assert.equal(await tokenHolderInstance.isRequirementAchieved(transactionId),false);
-		
-		// Transaction proposed and final requirement will be achieved.
-		await tokenHolderInstance.authorizeSession(newAccount.address, 100, blockNumber + 50,{from: accounts[6]});
-		assert.equal(await tokenHolderInstance.isRequirementAchieved(transactionId),true);
-	});
-	
-	it('should verify ephemeral keys ', async () => {
-		
-		let ephemeralKeyData = await tokenHolderInstance.ephemeralKeys(newAccount.address);
-		
-		// checking whether ephemeral key is present
-		assert(ephemeralKeyData[0].toNumber() > 0);
-		assert.isTrue(ephemeralKeyData[0].toNumber() > 0);
-		
-		
-	});
-	
-	
+  const openSTProtocol = accounts[0],
+    conversionRateDecimals = 5,
+    conversionRate = new BigNumber(10 * 10 ** conversionRateDecimals),
+    chainIDValue = 3,
+    chainIDUtility = 1410,
+    symbol = 'symbol',
+    name = 'name',
+    organizationAddress = accounts[1],
+    token = null;
+  let tokenHolderInstance, newAccount, blockNumber;
+  beforeEach(async () => {
+    const hasher = await Hasher.new();
+    const tokenRules = accounts[0];
+    const valueToken = accounts[1];
+    brandedTokenInstance = await brandedToken.new(
+      valueToken,
+      symbol,
+      name,
+      18,
+      chainIDValue,
+      chainIDUtility,
+      conversionRate,
+      conversionRateDecimals,
+      organizationAddress,
+      { from: openSTProtocol }
+    );
+  });
+
+  it('should deploy token holder with two requirements', async () => {
+    tokenHolderInstance = await tokenHolder.new(brandedTokenInstance.address, accounts[3], accounts[1], 2, [
+      accounts[7],
+      accounts[6]
+    ]);
+
+    assert.equal(await tokenHolderInstance.isWallet(accounts[6]), true);
+    assert.equal(await tokenHolderInstance.isWallet(accounts[4]), false);
+  });
+
+  it('should authorizeSession for an ephemeral key with two requirements ', async () => {
+    //newAccount = web3.eth.accounts.create(web3.utils.randomHex(32));
+    let transactionId = await tokenHolderInstance.authorizeSession.call(
+      accounts[3],
+      100,
+      (await web3.eth.getBlockNumber()) + 50,
+      { from: accounts[7] }
+    );
+
+    blockNumber = await web3.eth.getBlockNumber();
+
+    // Transaction proposed and requirement achieved is 1.
+    await tokenHolderInstance.authorizeSession(accounts[3], 100, blockNumber + 50, { from: accounts[7] });
+    assert.equal(await tokenHolderInstance.isRequirementAchieved(transactionId), false);
+
+    // Transaction proposed and final requirement will be achieved.
+    await tokenHolderInstance.authorizeSession(accounts[3], 100, blockNumber + 50, { from: accounts[6] });
+    assert.equal(await tokenHolderInstance.isRequirementAchieved(transactionId), true);
+  });
+
+  it('should verify ephemeral keys ', async () => {
+    let ephemeralKeyData = await tokenHolderInstance.ephemeralKeys(accounts[3]);
+
+    // checking whether ephemeral key is present
+    assert(ephemeralKeyData[0].toNumber() > 0);
+    assert.isTrue(ephemeralKeyData[0].toNumber() > 0);
+  });
+
+  it('should execute an rule', async () => {
+    let ephemeralKeyData = await tokenHolderInstance.ephemeralKeys(accounts[3]);
+
+    let signer = accounts[3],
+      msgToBeSigned = web3Utils.soliditySha3(
+        { t: 'bytes', v: '0x19' },
+        { t: 'bytes', v: '0x00' },
+        { t: 'address', v: tokenHolderInstance.address },
+        { t: 'address', v: '0xdca86553f179820bf7a17b4eb9a9d52fe92013b4' },
+        { t: 'uint8', v: 0 },
+        { t: 'bytes', v: '0xdca86553f179820bf7a17b4eb9a9d52fe92013b4' },
+        { t: 'uint256', v: ephemeralKeyData[1].toNumber() },
+        { t: 'uint8', v: 0 },
+        { t: 'uint8', v: 0 },
+        { t: 'uint8', v: 0 },
+        { t: 'bytes4', v: '0xdca86553' },
+        { t: 'uint8', v: 0 },
+        { t: 'bytes', v: '' }
+      );
+
+    let signature = await web3.eth.sign(msgToBeSigned, signer);
+
+    signature = signature.slice(2);
+    const r = '0x' + signature.slice(0, 64);
+    const s = '0x' + signature.slice(64, 128);
+    const v = '0x' + signature.slice(128, 130);
+    let v_decimal = web3.utils.toDecimal(v) + 27;
+
+    let transaction = await tokenHolderInstance.executeRule(
+      tokenHolderInstance.address,
+      '0xdca86553f179820bf7a17b4eb9a9d52fe92013b4',
+      ephemeralKeyData[1].toNumber(),
+      '0xdca86553f179820bf7a17b4eb9a9d52fe92013b4',
+      v_decimal,
+      r,
+      s,
+      '0xdca86553'
+    );
+  });
 };
